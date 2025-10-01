f22de88fd961da17fba8a8a9b4f6212046774c1d
default <nicholas.sun@stu.ocsb.ca>
2025-10-01 13:55:17 +0000

    Created programs to print the author's name.

---
+++ main.go
@@ -0,0 +1,16 @@
+/**
+ * @author Nicholas
+ * @version 1.0.0
+ * @date 2025-10-01
+ * @fileoverview This program prints the author's name.
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Nicholas Sun")
+
+	fmt.Println("\nDone.")
+}
---
+++ main.ts
@@ -0,0 +1,11 @@
+/**
+ * @author Nicholas
+ * @version 1.0.0
+ * @date 2025-10-01
+ * @fileoverview This program prints the author's name.
+ */
+
+// print out the author's name.
+console.log("Nicholas Sun");
+
+console.log("\nDone.");
